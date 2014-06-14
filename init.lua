56ada588119e152eaf59a5e43dde9ac42a4db058
Joshua Hopp <JoshuaHopp@web.de>
2014-06-14 20:46:12 +0200

    pushstone working

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
--- MOVESTONE
+
 -- Non-sticky:
 -- Moves along mesecon lines
 -- Pushes all blocks in front of it
@@ -8,106 +8,79 @@
 -- Pushes all block in front of it
 -- Pull all blocks in its back
 
-function mesecon:get_movestone_direction(pos)
-	getactivated = 0
-	local lpos
-	local getactivated = 0
-	local rules = {
-		{x=0,  y=1,  z=-1},
-		{x=0,  y=0,  z=-1},
-		{x=0,  y=-1, z=-1},
-		{x=0,  y=1,  z=1},
-		{x=0,  y=-1, z=1},
-		{x=0,  y=0,  z=1},
-		{x=1,  y=0,  z=0},
-		{x=1,  y=1,  z=0},
-		{x=1,  y=-1, z=0},
-		{x=-1, y=1,  z=0},
-		{x=-1, y=-1, z=0},
-		{x=-1, y=0,  z=0}}
-
-	lpos = {x=pos.x+1, y=pos.y, z=pos.z}
-	for n = 1, 3 do
-		if mesecon:is_power_on(lpos, rules[n].x, rules[n].y, rules[n].z) then
-			return {x=0, y=0, z=-1}
-		end
-	end
-
-	lpos = {x = pos.x-1, y = pos.y, z = pos.z}
-	for n=4, 6 do
-		if mesecon:is_power_on(lpos, rules[n].x, rules[n].y, rules[n].z) then
-			return {x=0, y=0, z=1}
-		end
-	end
-
-	lpos = {x = pos.x, y = pos.y, z = pos.z+1}
-	for n=7, 9 do
-		if mesecon:is_power_on(lpos, rules[n].x, rules[n].y, rules[n].z) then
-			return {x=-1, y=0, z=0}
-		end
-	end
-
-	lpos = {x = pos.x, y = pos.y, z = pos.z-1}
-	for n=10, 12 do
-		if mesecon:is_power_on(lpos, rules[n].x, rules[n].y, rules[n].z) then
-			return {x=1, y=0, z=0}
-		end
-	end
+local function get_pushstone_direction(pos)
+	local node = minetest.get_node(pos)
+	return minetest.facedir_to_dir(node.param2)
 end
 
-minetest.register_node("mesecons_movestones:movestone", {
-	tiles = {"jeija_movestone_side.png", "jeija_movestone_side.png", "jeija_movestone_side.png", "jeija_movestone_side.png", "jeija_movestone_arrows.png", "jeija_movestone_arrows.png"},
+minetest.register_node("pushstone:pushstone", {
+	tiles = {"pushstone_arrows.png^[transformR90", "pushstone_arrows.png^[transformR270", "pushstone_arrows.png", "pushstone_arrows.png^[transformR180", "pushstone_side.png", "pushstone_side.png"},
 	paramtype2 = "facedir",
 	legacy_facedir_simple = true,
 	groups = {cracky=3},
-    	description="Movestone",
+    	description="Pushstone",
 	sounds = default.node_sound_stone_defaults(),
 	mesecons = {effector = {
 		action_on = function (pos, node)
-			local direction=mesecon:get_movestone_direction(pos)
+			local direction=get_pushstone_direction(pos)
 			if not direction then return end
 			minetest.remove_node(pos)
 			mesecon:update_autoconnect(pos)
-			minetest.add_entity(pos, "mesecons_movestones:movestone_entity")
+			local ent = minetest.add_entity(pos, "pushstone:pushstone_entity")
+			print("Nodedir: "..dump(direction))
+			ent:get_luaentity().direction = direction
 		end
 	}}
 })
 
-minetest.register_entity("mesecons_movestones:movestone_entity", {
+minetest.register_entity("pushstone:pushstone_entity", {
 	physical = false,
 	visual = "sprite",
-	textures = {"jeija_movestone_side.png", "jeija_movestone_side.png", "jeija_movestone_side.png", "jeija_movestone_side.png", "jeija_movestone_arrows.png", "jeija_movestone_arrows.png"},
+	textures = {"pushstone_arrows.png^[transformR90", "pushstone_arrows.png^[transformR270", "pushstone_arrows.png", "pushstone_arrows.png^[transformR180", "pushstone_side.png", "pushstone_side.png"},
 	collisionbox = {-0.5,-0.5,-0.5, 0.5, 0.5, 0.5},
 	visual = "cube",
 	lastdir = {x=0, y=0, z=0},
+	lastpos = nil,
 
 	on_punch = function(self, hitter)
 		self.object:remove()
-		hitter:get_inventory():add_item("main", "mesecons_movestones:movestone")
+		hitter:get_inventory():add_item("main", "pushstone:pushstone")
 	end,
 
 	on_step = function(self, dtime)
 		local pos = self.object:getpos()
-		pos.x, pos.y, pos.z = math.floor(pos.x+0.5), math.floor(pos.y+0.5), math.floor(pos.z+0.5)
-		local direction = mesecon:get_movestone_direction(pos)
 
-		if not direction then -- no mesecon power
+		if self.lastpos == nil then
+			self.lastpos =  {x=pos.x, y=pos.y, z=pos.z}
+		end
+
+		local finished = false
+		if math.abs(self.lastpos.x - pos.x) + math.abs(self.lastpos.y - pos.y) + math.abs(self.lastpos.z - pos.z) >= 1 then
+			finished = true
+		end
+
+		pos.x, pos.y, pos.z = math.floor(pos.x+0.5), math.floor(pos.y+0.5), math.floor(pos.z+0.5)
+		local direction = self.direction
+
+		print("entdir: "..dump(direction))
+
+		if finished then -- no mesecon power
 			--push only solid nodes
 			local name = minetest.get_node(pos).name
 			if  name ~= "air" and name ~= "ignore"
 			and ((not minetest.registered_nodes[name])
 			or minetest.registered_nodes[name].liquidtype == "none") then
-				mesecon:mvps_push(pos, self.lastdir, MOVESTONE_MAXIMUM_PUSH)
+				mesecon:mvps_push(pos, direction, MOVESTONE_MAXIMUM_PUSH)
 			end
-			minetest.add_node(pos, {name="mesecons_movestones:movestone"})
+			minetest.add_node(pos, {name="pushstone:pushstone", param2=minetest.dir_to_facedir(self.direction, true)})
 			self.object:remove()
 			return
 		end
 
 		local success, stack, oldstack =
 			mesecon:mvps_push(pos, direction, MOVESTONE_MAXIMUM_PUSH)
-		if not success then -- Too large stack/stopper in the way
-			minetest.add_node(pos, {name="mesecons_movestones:movestone"})
+		if finished or not success then -- Too large stack/stopper in the way
+			minetest.add_node(pos, {name="pushstone:pushstone", param2=minetest.dir_to_facedir(self.direction, true)})
 			self.object:remove()
 			return
 		else
@@ -121,11 +94,11 @@ minetest.register_entity("mesecons_movestones:movestone_entity", {
 })
 
 minetest.register_craft({
-	output = "mesecons_movestones:movestone 2",
+	output = "pushstone:pushstone 2",
 	recipe = {
-		{"default:stone", "default:stone", "default:stone"},
+		{"moreores:tin_ingot", "moreores:tin_ingot", "moreores:tin_ingot"},
 		{"group:mesecon_conductor_craftable", "group:mesecon_conductor_craftable", "group:mesecon_conductor_craftable"},
-		{"default:stone", "default:stone", "default:stone"},
+		{"moreores:tin_ingot", "moreores:tin_ingot", "moreores:tin_ingot"},
 	}
 })
 
@@ -133,69 +106,79 @@ minetest.register_craft({
 
 -- STICKY_MOVESTONE
 
-minetest.register_node("mesecons_movestones:sticky_movestone", {
-	tiles = {"jeija_movestone_side.png", "jeija_movestone_side.png", "jeija_movestone_side.png", "jeija_movestone_side.png", "jeija_sticky_movestone.png", "jeija_sticky_movestone.png"},
-	inventory_image = minetest.inventorycube("jeija_sticky_movestone.png", "jeija_movestone_side.png", "jeija_movestone_side.png"),
+minetest.register_node("pushstone:sticky_pushstone", {
+	tiles = {"sticky_pushstone.png^[transformR90", "sticky_pushstone.png^[transformR270", "sticky_pushstone.png", "sticky_pushstone.png^[transformR180", "pushstone_side.png", "pushstone_side.png"},
 	paramtype2 = "facedir",
 	legacy_facedir_simple = true,
 	groups = {cracky=3},
-    	description="Sticky Movestone",
+    	description="Sticky Pushstone",
 	sounds = default.node_sound_stone_defaults(),
 	mesecons = {effector = {
 		action_on = function (pos, node)
-			local direction=mesecon:get_movestone_direction(pos)
+			local direction=get_pushstone_direction(pos)
 			if not direction then return end
 			minetest.remove_node(pos)
 			mesecon:update_autoconnect(pos)
-			minetest.add_entity(pos, "mesecons_movestones:sticky_movestone_entity")
+			local ent = minetest.add_entity(pos, "pushstone:sticky_pushstone_entity")
+			ent:get_luaentity().direction = direction
 		end
 	}}
 })
 
 minetest.register_craft({
-	output = "mesecons_movestones:sticky_movestone 2",
+	output = "pushstone:sticky_pushstone 2",
 	recipe = {
-		{"mesecons_materials:glue", "mesecons_movestones:movestone", "mesecons_materials:glue"},
+		{"mesecons_materials:glue", "pushstone:pushstone", "mesecons_materials:glue"},
 	}
 })
 
-minetest.register_entity("mesecons_movestones:sticky_movestone_entity", {
+minetest.register_entity("pushstone:sticky_pushstone_entity", {
 	physical = false,
 	visual = "sprite",
-	textures = {"jeija_movestone_side.png", "jeija_movestone_side.png", "jeija_movestone_side.png", "jeija_movestone_side.png", "jeija_sticky_movestone.png", "jeija_sticky_movestone.png"},
+	textures = {"sticky_pushstone.png^[transformR90", "sticky_pushstone.png^[transformR270", "sticky_pushstone.png", "sticky_pushstone.png^[transformR180", "pushstone_side.png", "pushstone_side.png"},
 	collisionbox = {-0.5, -0.5, -0.5, 0.5, 0.5, 0.5},
 	visual = "cube",
 	lastdir = {x=0, y=0, z=0},
 
 	on_punch = function(self, hitter)
 		self.object:remove()
-		hitter:get_inventory():add_item("main", 'mesecons_movestones:sticky_movestone')
+		hitter:get_inventory():add_item("main", 'pushstone:sticky_pushstone')
 	end,
 
 	on_step = function(self, dtime)
 		local pos = self.object:getpos()
-		pos.x, pos.y, pos.z = math.floor(pos.x+0.5), math.floor(pos.y+0.5), math.floor(pos.z+0.5)
-		local direction = mesecon:get_movestone_direction(pos)
 
-		if not direction then -- no mesecon power
+		if self.lastpos == nil then
+			self.lastpos =  {x=pos.x, y=pos.y, z=pos.z}
+		end
+
+		local finished = false
+		if math.abs(self.lastpos.x - pos.x) + math.abs(self.lastpos.y - pos.y) + math.abs(self.lastpos.z - pos.z) >= 1 then
+			finished = true
+		end
+
+		pos.x, pos.y, pos.z = math.floor(pos.x+0.5), math.floor(pos.y+0.5), math.floor(pos.z+0.5)
+		local direction = self.direction
+
+		print("entdir: "..dump(direction))
+
+		if finished then -- no mesecon power
 			--push only solid nodes
 			local name = minetest.get_node(pos).name
 			if  name ~= "air" and name ~= "ignore"
 			and ((not minetest.registered_nodes[name])
 			or minetest.registered_nodes[name].liquidtype == "none") then
-				mesecon:mvps_push(pos, self.lastdir, MOVESTONE_MAXIMUM_PUSH)
-				--STICKY
-				mesecon:mvps_pull_all(pos, self.lastdir)
+				mesecon:mvps_push(pos, direction, MOVESTONE_MAXIMUM_PUSH)
 			end
-			minetest.add_node(pos, {name="mesecons_movestones:sticky_movestone"})
+			minetest.add_node(pos, {name="pushstone:sticky_pushstone", param2=minetest.dir_to_facedir(self.direction, true)})
 			self.object:remove()
 			return
 		end
 
 		local success, stack, oldstack =
 			mesecon:mvps_push(pos, direction, MOVESTONE_MAXIMUM_PUSH)
-		if not success then -- Too large stack/stopper in the way
-			minetest.add_node(pos, {name="mesecons_movestones:sticky_movestone"})
+		if finished or not success then -- Too large stack/stopper in the way
+			minetest.add_node(pos, {name="pushstone:sticky_pushstone", param2=minetest.dir_to_facedir(self.direction, true)})
 			self.object:remove()
 			return
 		else
@@ -212,5 +195,5 @@ minetest.register_entity("mesecons_movestones:sticky_movestone_entity", {
 })
 
 
-mesecon:register_mvps_unmov("mesecons_movestones:movestone_entity")
-mesecon:register_mvps_unmov("mesecons_movestones:sticky_movestone_entity")
+mesecon:register_mvps_unmov("pushstone:pushstone_entity")
+mesecon:register_mvps_unmov("pushstone:sticky_pushstone_entity")
